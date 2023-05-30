203b547bb0690ce734a382b5874ee13605144137
oguzhancakal <oguzhncakal@gmail.com>
2023-05-30 16:33:59 +0300

    Products Screen

---
+++ App.js
@@ -0,0 +1,23 @@
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, View } from "react-native";
+import ProductScreen from "./src/screens/ProductsScreen";
+import ProductDetailsScreen from "./src/screens/ProductDetailsScreen";
+
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+     {/* <ProductScreen/> */}
+     <ProductDetailsScreen/>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    
+  },
+});

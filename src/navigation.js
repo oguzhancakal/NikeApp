5bffd57a0d3fba98ee3cbadd8100d10e060d9322
oguzhancakal <oguzhncakal@gmail.com>
2023-05-31 14:55:23 +0300

    Cart Redux Slice

--- src/navigation.js
+++ src/navigation.js
@@ -6,10 +6,14 @@ import ProductDetailsScreen from "./screens/ProductDetailsScreen";
 import ShoppingCart from "./screens/ShoppingCart";
 import { Pressable,Text } from "react-native";
 import { FontAwesome5 } from "@expo/vector-icons";
+import { useSelector } from "react-redux";
+import { selecetNumberOfItems } from "./store/cartSlice";
+
 
 const Stack = createNativeStackNavigator();
 
 const Navigation = () => {
+  const numberOfItems = useSelector(selecetNumberOfItems);
   return (
     <NavigationContainer>
       <Stack.Navigator screenOptions={{contentStyle: {backgroundColor:"white"}}}>
@@ -23,7 +27,7 @@ const Navigation = () => {
                 style={{flexDirection:"row"}}
                 >
                     <FontAwesome5 name="shopping-cart" size={18} color="gray" />
-                    <Text style={{marginLeft:5, fontWeight:"500"}}>1</Text>
+                    <Text style={{marginLeft:5, fontWeight:"500"}}>{numberOfItems}</Text>
                 </Pressable>    
             ),
          })}
--- src/screens/ProductDetailsScreen.js
+++ src/screens/ProductDetailsScreen.js
@@ -9,16 +9,20 @@ import {
   Pressable,
 } from "react-native";
 import products from "../data/products";
-import { useSelector } from "react-redux";
+import { useSelector, useDispatch } from "react-redux";
+import { cartSlice } from "../store/cartSlice";
+
 
 
 const ProductDetailsScreen = () => {
   const product = useSelector((state)=> state.products.selectedProduct);
 
+  const dispatch = useDispatch();
+
   const { width } = useWindowDimensions();
 
-  const addToCart =()=>{
-    console.warn("Add to cart");
+  const addToCart = () => {
+    dispatch(cartSlice.actions.addCartItem({product:product}));
   };
 
   return (
